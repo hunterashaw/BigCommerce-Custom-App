321685595e1dbad077b48ee6ba1c27fc92223e53
Hunter Shaw <hunter.shaw@classyllama.com>
2020-10-12 08:23:06 -0500

    Added body-parser
    For POST requests allowing the req.body element to be available.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const app = require('polka')()
+app.use(require('body-parser').json())
 
 //const bc = require('./helpers/bigCommerce'), error = require('./helpers/errors'), db = require('./helpers/db')
 
